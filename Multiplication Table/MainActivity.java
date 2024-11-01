37d50e331b40f676d9a534866d28219ed980377f
MIDHUN R.S <157745973+Midhun-r-s@users.noreply.github.com>
2024-11-01 10:06:49 +0530

    Update MainActivity.java

--- Multiplication Table/MainActivity.java
+++ Multiplication Table/MainActivity.java
@@ -1,1 +1,44 @@
+package com.example.multiplicationtable;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+EditText number,range;
+Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+     number=(EditText) findViewById(R.id.number);
+     range= (EditText) findViewById(R.id.range);
+     button=(Button) findViewById(R.id.button);
+
+
+     button.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             if (TextUtils.isEmpty(number.getText()) || TextUtils.isEmpty(range.getText())) {
+                 Toast.makeText(MainActivity.this, "Please Enter the number and range", Toast.LENGTH_SHORT).show();
+             }
+             else {
+                 Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                 String num = number.getText().toString();
+                 String r = range.getText().toString();
+                 i.putExtra("number", num);
+                 i.putExtra("range", r);
+                 startActivity(i);
+             }
+         }
+     });
+
+
+    }
+}
